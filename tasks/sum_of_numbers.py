c1c7aabaaa113934a4b5a574f98d1b1edbc15f19
Soloparame <yihenewrebika@gmail.com>
2025-04-19 10:27:47 -0700

    newAPI

---
+++ tasks/sum_of_numbers.py
@@ -0,0 +1,9 @@
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the sum
+print(f"The sum of {num1} and {num2} is: {sum}")
